f831f8053c5378e4f8b5d92ec497cae0e05acd3b
noemi <noemi@vbot.tv>
2017-04-30 16:05:47 +0100

    last AR version

--- Satellite click phone/Assets/scripts/SatBehavour.cs
+++ Satellite click phone/Assets/scripts/SatBehavour.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class SatBehavour : MonoBehaviour {
     float rotationSpeed = 5.0f; //degrees per second
     string loc = "?=0,0";
+    public GameObject satellite;
 
 	// Use this for initialization
 	void Start () {
@@ -23,10 +24,13 @@ public class SatBehavour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(new Vector3(
-           0 * rotationSpeed * Time.deltaTime,
+        //transform.LookAt(Vector3.zero);
+
+        satellite.transform.Rotate(new Vector3(
+           1f * rotationSpeed * Time.deltaTime,
                1f * rotationSpeed * Time.deltaTime,
-               1f * rotationSpeed * Time.deltaTime));
+               0 * rotationSpeed * Time.deltaTime));
+
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("Mouse is down");
@@ -56,46 +60,6 @@ public class SatBehavour : MonoBehaviour {
             }
             Debug.Log("Mouse is down");
         }
-    }
-
-    IEnumerator CheckLoc()
-    {
-        // First, check if user has location service enabled
-        if (!Input.location.isEnabledByUser)
-            yield break;
-
-        // Start service before querying location
-        Input.location.Start();
-
-        // Wait until service initializes
-        int maxWait = 20;
-        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
-        {
-            yield return new WaitForSeconds(1);
-            maxWait--;
-        }
-
-        // Service didn't initialize in 20 seconds
-        if (maxWait < 1)
-        {
-            Debug.Log("Timed out");
-            yield break;
-        }
-
-        // Connection has failed
-        if (Input.location.status == LocationServiceStatus.Failed)
-        {
-            Debug.Log("Unable to determine device location");
-            yield break;
-        }
-        else
-        {
-            // Access granted and location value could be retrieved
-            Debug.Log("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
-        }
-
-        // Stop service if there is no need to query location updates continuously
-        Input.location.Stop();
     }
 
 }
